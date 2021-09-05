17c13f8968079b6eb5a4a7e7395965f6afbb275f
spanyell <67851763+spanyell@users.noreply.github.com>
2021-09-05 16:54:23 -0700

    Initial Commit

---
+++ LearnMusicApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnMusicApp
+//
+//  Created by Dan Beers on 9/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnMusicApp/LearnMusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnMusicAppApp.swift
+//  LearnMusicApp
+//
+//  Created by Dan Beers on 9/5/21.
+//
+
+import SwiftUI
+
+@main
+struct LearnMusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
