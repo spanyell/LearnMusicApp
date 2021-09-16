f1df5885ee762f6b04213436dfa55b3319c9acc2
spanyell <67851763+spanyell@users.noreply.github.com>
2021-09-15 17:17:09 -0700

    Adding all the music and pics.

--- LearnMusicApp/Constants.swift
+++ LearnMusicApp/Constants.swift
@@ -6,3 +6,5 @@
 //
 
 import Foundation
+
+// Every picture file needs to be 300x300. Use the snipping tool CMD+Shift+4 and measure out 150x150 to get 300x300px. Make sure the measure lines reach entire width.
